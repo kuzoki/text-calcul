49c6684d7c040b0330e7c7901abd7f576450e663
kuzoki <kuzokio4@gmail.com>
2021-05-06 01:24:20 +0000

    NEW OPTION FILLTER WORD

--- app.js
+++ app.js
@@ -7,14 +7,16 @@
 
 (function($){
 "use strict";
-	
+$(".s-result" ).hide(); 	
 	
 
 	$( ".calculate-words" ).on( "click", function() {
 	  	let texta = $('.textarea').val();
 		let block = [];
 		let characnb = [];
-
+		
+		 console.log(block)
+		
 		const words = texta.split(' ');
 		
 		   for (var i = 0; i < words.length; i++) {
@@ -38,7 +40,37 @@
 		  }else {
 		  	$(".w-number").text(block.length);
 		   	$(".c-number").text(characnb.length);
+		   	$(".search" ).show(); 
 		  }
+		
+		$(".text p" ).html( texta );
+		
 	});
+
+	
+
+	$( ".search" ).on( "click", function() {
+
+		var textarea = $( ".text p" ).text();
+		let inputVal = $(".input-search").val();
+
+		//console.log(inputVal);
+		if (inputVal != "") {
+			function checkWord(e) {
+			  return e == inputVal;
+			};
+		  	const n = textarea.split(' ');
+		  	let narr = n.filter(checkWord);
+
+		  	$(".repeated").text(narr.length);
+		  	$(".word-va").text(inputVal);
+		  	$(".s-result" ).show(); 
+
+		}else{
+			alert("EMPTY INPUT");
+		}
+		
+	});
+	
 
 })(jQuery);
